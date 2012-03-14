a2f4a1c1d280cb300518b36b24475a70e4d86841
Irving Ruan <irvingruan@gmail.com>
2012-03-13 20:29:31 -0700

    Added base conditions

--- Moniker.py
+++ Moniker.py
@@ -14,19 +14,48 @@
 
 import os
 import sys
-
+import getopt
 
 def usage():
 	sys.stderr.write("Usage: ./Moniker.py -p [path] -c [case]\n")
-	sys.stderr.write("Type './Moniker.py --help' for more info\n")
+	sys.stderr.write("Type `./Moniker.py --help` for more info\n")
 	
 def help():
 	sys.stderr.write("Flags of Moniker are:\n")
-	sys.stderr.write("\t-p\t[path] The directory path to search for unformatted directories\n")
-	sys.stderr.write("\t-c\t[case] Flag to set upper or lowercase formatting for directories\n\n")
+	sys.stderr.write(" -p [path] The directory path to search for unformatted directories\n")
+	sys.stderr.write(" -c [case: Upper | Lower] Flag to set upper or lowercase formatting for directories\n\n")
 	sys.stderr.write("Example: ./Moniker.py -p ~/Documents/ -c Upper\n")
 	sys.stderr.write("e.g. Find all directories in ~/Documents and change them to proper uppercase for the first letter\n")
 
-
+def main():
+	if len(sys.argv) != 5:	
+		if len(sys.argv) == 2 and (sys.argv[1] == "--help" or sys.argv[1] == "--h"):
+			help()
+			return False
+		else:
+			usage()
+			return False
+			
+	path = sys.argv[2]
+	case = sys.argv[4]
+	
+	# Check validity of path provided
+	if not(os.path.isdir(path)):
+		sys.stderr.write("Error: '" + path + "' is an invalid path\n")
+		usage()
+		return False
+		
+	# Make sure the case flag is valid
+	if case.lower() != "upper" and case.lower() != "lower":
+		sys.stderr.write("Error: '" + case + "' is an invalid case flag\n")
+		usage()
+		return False
+		
+	
+		
+		
+		
 if __name__ == "__main__":
+	main()
+	
 	
