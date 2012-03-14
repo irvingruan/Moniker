9c0dd10fd355467141fd811facef9bcd1b55f27b
Irving Ruan <irvingruan@gmail.com>
2012-03-13 19:46:35 -0700

    Added base files

---
+++ Moniker.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# Author: Irving Ruan
+# Contact: irvingruan@gmail.com
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+
+
+def usage():
+	sys.stderr.write("Usage: ./Moniker.py -p [path] -c [case]\n")
+	sys.stderr.write("Type './Moniker.py --help' for more info\n")
+	
+def help():
+	sys.stderr.write("Flags of Moniker are:\n")
+	sys.stderr.write("\t-p\t[path] The directory path to search for unformatted directories\n")
+	sys.stderr.write("\t-c\t[case] Flag to set upper or lowercase formatting for directories\n\n")
+	sys.stderr.write("Example: ./Moniker.py -p ~/Documents/ -c Upper\n")
+	sys.stderr.write("e.g. Find all directories in ~/Documents and change them to proper uppercase for the first letter\n")
+
+
+if __name__ == "__main__":
+	
